f468964df8bc3d0e6965ef04afd1be88d174e131
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-11-18 09:27:07 +0100

    first commit

---
+++ src/main/java/fr/isika/al17/raiddonspringserver/RaiddonSpringServerApplication.java
@@ -0,0 +1,13 @@
+package fr.isika.al17.raiddonspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiddonSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaiddonSpringServerApplication.class, args);
+	}
+
+}
