7f97f62e76ba7ed14a30312c0b34d9e622638d55
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-11-30 11:21:49 +0100

    log in sign up okay

--- src/main/java/fr/isika/al17/raiddonspringserver/repository/PlayableCharacterRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.isika.al17.raiddonspringserver.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.isika.al17.raiddonspringserver.models.PlayableCharacter;
-
-public interface PlayableCharacterRepository extends JpaRepository<PlayableCharacter, Long>{
-
-}
--- src/main/java/fr/isika/al17/raiddonspringserver/repository/UserRepository.java
+++ src/main/java/fr/isika/al17/raiddonspringserver/repository/UserRepository.java
@@ -1,15 +1,17 @@
 package fr.isika.al17.raiddonspringserver.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.isika.al17.raiddonspringserver.models.User;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByUsername(String username);
 
-public interface UserRepository extends JpaRepository<User, Long>{
-    
-    User findByUsername (String username);
-    
-    User findByEmail (String email);
-    
-    User findByBattleTag (String battleTag);
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
 }
