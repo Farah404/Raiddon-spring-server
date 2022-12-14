8d427f5c897598973dcff427b2b08f6eeee0e299
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-12-14 13:07:29 +0100

    update user profile v9

--- src/main/java/fr/isika/al17/raiddonspringserver/controllers/UserController.java
+++ src/main/java/fr/isika/al17/raiddonspringserver/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.isika.al17.raiddonspringserver.models.User;
 import fr.isika.al17.raiddonspringserver.models.UserDTO;
 import fr.isika.al17.raiddonspringserver.repository.UserRepository;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class UserController implements HealthIndicator {
